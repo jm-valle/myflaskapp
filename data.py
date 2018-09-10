6c7608624f99ed699caeaffa3061b4437be81c3f
jm-valle <jmarcos.araujo96@gmail.com>
2018-09-09 23:43:44 -0300

    to merge

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'Is this real life, or is this fantasy',
+            'author' : 'Joao Marcos',
+            'create_date' : '05-09-2018'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : 'Is this real life, or is this fantasy',
+            'author' : 'John Doe',
+            'create_date' : '05-09-2018'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : 'Is this real life, or is this fantasy',
+            'author' : 'Joao Marcos',
+            'create_date' : '05-09-2018'
+        }
+    ]
+    return articles
